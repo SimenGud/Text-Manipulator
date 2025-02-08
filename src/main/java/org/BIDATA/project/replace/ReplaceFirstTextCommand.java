9f145abf99a8365367b2091ba96c48b22234304c
SimenGud <simeng2003@gmail.com>
2025-02-08 02:53:58 +0100

    Made two methods, ReplaceTextCommand and ReplaceFirstTextCommand

--- src/main/java/org/BIDATA/project/replace/ReplaceFirstTextCommand.java
+++ src/main/java/org/BIDATA/project/replace/ReplaceFirstTextCommand.java
@@ -1,4 +1,13 @@
 package org.BIDATA.project.replace;
 
 public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replaceFirst(target, super.replacement);
+  }
 }
--- src/main/java/org/BIDATA/project/replace/ReplaceTextCommand.java
+++ src/main/java/org/BIDATA/project/replace/ReplaceTextCommand.java
@@ -1,4 +1,20 @@
 package org.BIDATA.project.replace;
 
-public class ReplaceTextCommand {
+
+import org.BIDATA.project.text.TextCommand;
+
+public class ReplaceTextCommand implements TextCommand {
+  protected final String target;
+  protected final String replacement;
+
+  public ReplaceTextCommand(String target, String replacement) {
+    this.target = target;
+    this.replacement = replacement;
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replaceAll(target, replacement);
+  }
 }
+
