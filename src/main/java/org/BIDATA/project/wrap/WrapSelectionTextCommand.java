e30865b00121e1ea4faffc47b13a79eb82fced5e
SimenGud <simeng2003@gmail.com>
2025-02-08 15:49:16 +0100

    Improved WrapSelection method and test

--- src/main/java/org/BIDATA/project/wrap/WrapSelectionTextCommand.java
+++ src/main/java/org/BIDATA/project/wrap/WrapSelectionTextCommand.java
@@ -2,26 +2,15 @@ package org.BIDATA.project.wrap;
 
 public class WrapSelectionTextCommand extends WrapTextCommand {
 
-  /**
-   * Wrap the selected text with the given start and end strings.
-   *
-   * @param start the start string
-   * @param end the end string
-   */
+  protected String selection;
 
-  public WrapSelectionTextCommand(String start, String end) {
+  public WrapSelectionTextCommand(String start, String end, String selection) {
     super(start, end);
+    this.selection = selection;
   }
-
-  /**
-   * Wraps the input text using the start and end strings.
-   *
-   * @param text the text to be modified
-   * @return the modified text
-   */
 
   @Override
   public String execute(String text) {
-    return start + text + end;
+    return text.replaceFirst(selection, start + selection + end);
   }
 }
--- src/test/java/org/BIDATA/project/wrap/WrapSelectionTextCommandTest.java
+++ src/test/java/org/BIDATA/project/wrap/WrapSelectionTextCommandTest.java
@@ -1,4 +1,18 @@
 package org.BIDATA.project.wrap;
 
+import org.BIDATA.project.text.TextCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WrapSelectionTextCommandTest {
+
+  @Test
+  void testWrapSelection() {
+    TextCommand cmd = new WrapSelectionTextCommand("<p>", "</p>", "test");
+
+    String input = "This is test text.";
+    String expectedOutput = "This is <p>test</p> text.";
+
+    assertEquals(expectedOutput, cmd.execute(input));
+  }
 }
