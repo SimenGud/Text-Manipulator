140f0ac25ea59567a9a805aad32575b13690541f
SimenGud <simeng2003@gmail.com>
2025-02-08 03:04:31 +0100

    Added doc for ReplaceFirstTextCommand and ReplaceTextCommand class

--- src/main/java/org/BIDATA/project/replace/ReplaceFirstTextCommand.java
+++ src/main/java/org/BIDATA/project/replace/ReplaceFirstTextCommand.java
@@ -1,10 +1,34 @@
 package org.BIDATA.project.replace;
 
+/**
+ * The ReplaceFirstTextCommand is a subclass of
+ * the ReplaceTextCommand that replaces the first
+ * occurrence of the target text in the input
+ * text with the replacement text.
+ *
+ * @version 0.1
+ * @author Simen
+ */
+
 public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+
+  /**
+   * Constructor for ReplaceFirstTextCommand.
+   *
+   * @param target the string to be replaced
+   * @param replacement the string to replace the target with
+   */
 
   public ReplaceFirstTextCommand(String target, String replacement) {
     super(target, replacement);
   }
+
+  /**
+   * Replaces the first occurrence of the target string with the replacement string.
+   *
+   * @param text the text to be modified
+   * @return the modified text
+   */
 
   @Override
   public String execute(String text) {
--- src/main/java/org/BIDATA/project/replace/ReplaceTextCommand.java
+++ src/main/java/org/BIDATA/project/replace/ReplaceTextCommand.java
@@ -1,16 +1,37 @@
 package org.BIDATA.project.replace;
 
-
 import org.BIDATA.project.text.TextCommand;
+
+/**
+ * ReplaceTextCommand is a class that replaces all
+ * occurrences of a target string with a replacement string.
+ *
+ * @version 0.1
+ * @author Simen
+ */
 
 public class ReplaceTextCommand implements TextCommand {
   protected final String target;
   protected final String replacement;
 
+  /**
+   * Constructor for ReplaceTextCommand.
+   *
+   * @param target the string to be replaced
+   * @param replacement the string to replace the target with
+   */
+
   public ReplaceTextCommand(String target, String replacement) {
     this.target = target;
     this.replacement = replacement;
   }
+
+  /**
+   * Replaces all occurrences of the target string with the replacement string.
+   *
+   * @param text the text to be modified
+   * @return the modified text
+   */
 
   @Override
   public String execute(String text) {
