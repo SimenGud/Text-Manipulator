8977e891b5f4d24e988ce498591c4acfe87dc1fc
SimenGud <simeng2003@gmail.com>
2025-02-08 15:58:18 +0100

    Improvements to ReplaceText testing class

--- src/test/java/org/BIDATA/project/replace/ReplaceTextCommandTest.java
+++ src/test/java/org/BIDATA/project/replace/ReplaceTextCommandTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReplaceTextCommandTest {
   @Test
   void testReplace() {
-    TextCommand cmd = new ReplaceTextCommand("one", "two");
-    assertEquals("text with two and two", cmd.execute("text with one and one"));
+    TextCommand command = new ReplaceTextCommand("one", "two");
+
+    String input = "text with one and one";
+    String expectedOutput = "text with two and two";
+
+    assertEquals(expectedOutput, command.execute(input));
   }
 }
 
