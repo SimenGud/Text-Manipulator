6a3b842735daf66f9d12604517736a7a4db9d39d
SimenGud <simeng2003@gmail.com>
2025-02-08 15:50:24 +0100

    Added docs to WrapSelection

--- src/main/java/org/BIDATA/project/wrap/WrapSelectionTextCommand.java
+++ src/main/java/org/BIDATA/project/wrap/WrapSelectionTextCommand.java
@@ -1,13 +1,39 @@
 package org.BIDATA.project.wrap;
 
+/**
+ * A subclass of WrapTextCommand that wraps a selection of text.
+ *
+ * @version 0.2
+ * @author Simen
+ */
+
 public class WrapSelectionTextCommand extends WrapTextCommand {
 
+  /**
+   * The selection to wrap.
+   */
+
   protected String selection;
+
+  /**
+   * Constructor for WrapSelectionTextCommand.
+   *
+   * @param start the string to be added at the start of the selection
+   * @param end the string to be added at the end of the selection
+   * @param selection the selection to wrap
+   */
 
   public WrapSelectionTextCommand(String start, String end, String selection) {
     super(start, end);
     this.selection = selection;
   }
+
+  /**
+   * Wraps the selection of the input text using the start and end strings.
+   *
+   * @param text the text to be modified
+   * @return the modified text
+   */
 
   @Override
   public String execute(String text) {
