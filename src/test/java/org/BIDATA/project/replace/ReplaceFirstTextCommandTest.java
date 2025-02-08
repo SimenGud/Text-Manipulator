734c09a3b2d1deba2d1af0c2d1088b2d7407ab17
SimenGud <simeng2003@gmail.com>
2025-02-08 04:36:20 +0100

    Made succsessfull Replace testing classes

--- src/test/java/org/BIDATA/project/replace/ReplaceFirstTextCommandTest.java
+++ src/test/java/org/BIDATA/project/replace/ReplaceFirstTextCommandTest.java
@@ -1,4 +1,14 @@
 package org.BIDATA.project.replace;
 
+import org.BIDATA.project.text.TextCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReplaceFirstTextCommandTest {
+
+  @Test
+  void testReplace() {
+    TextCommand cmd = new ReplaceFirstTextCommand("one", "two");
+    assertEquals("text with two and one", cmd.execute("text with one and one"));
+  }
 }
--- src/test/java/org/BIDATA/project/replace/ReplaceTextCommandTest.java
+++ src/test/java/org/BIDATA/project/replace/ReplaceTextCommandTest.java
@@ -1,4 +1,14 @@
 package org.BIDATA.project.replace;
 
-public class ReplaceTextCommandTest {
+import org.BIDATA.project.text.TextCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplaceTextCommandTest {
+  @Test
+  void testReplace() {
+    TextCommand cmd = new ReplaceTextCommand("one", "two");
+    assertEquals("text with two and two", cmd.execute("text with one and one"));
+  }
 }
+
