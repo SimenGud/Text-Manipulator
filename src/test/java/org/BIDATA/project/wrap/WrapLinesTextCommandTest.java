042be42b4e9910056e4a07c13f13d8d2a6485877
SimenGud <simeng2003@gmail.com>
2025-02-08 14:50:24 +0100

    Testing to WrapLinesText class

--- src/test/java/org/BIDATA/project/wrap/WrapLinesTextCommandTest.java
+++ src/test/java/org/BIDATA/project/wrap/WrapLinesTextCommandTest.java
@@ -1,4 +1,19 @@
 package org.BIDATA.project.wrap;
 
-public class WrapLinesTextCommandTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WrapLinesTextCommandTest {
+
+  @Test
+  void testWrapMultipleLines() {
+    WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "</p>");
+
+    String input = "first line\nsecond line";
+    String expected = "<p>first line</p>\n<p>second line</p>";
+
+    String output = command.execute(input);
+
+    assertEquals(expected, output);
+  }
 }
