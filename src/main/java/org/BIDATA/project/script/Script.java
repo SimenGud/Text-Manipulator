a41956b3719470bbc6bece56237f31dc46b1f7dc
SimenGud <simeng2003@gmail.com>
2025-02-08 04:27:57 +0100

    Added script method

--- src/main/java/org/BIDATA/project/script/Script.java
+++ src/main/java/org/BIDATA/project/script/Script.java
@@ -1,4 +1,20 @@
 package org.BIDATA.project.script;
 
-public class Script {
+import org.BIDATA.project.text.TextCommand;
+import java.util.List;
+
+public class Script implements TextCommand {
+  private final List<TextCommand> commands;
+
+  public Script(List<TextCommand> commands) {
+    this.commands = commands;
+  }
+
+  @Override
+  public String execute(String text) {
+    for (TextCommand command : commands) {
+      text = command.execute(text);
+    }
+    return text;
+  }
 }
