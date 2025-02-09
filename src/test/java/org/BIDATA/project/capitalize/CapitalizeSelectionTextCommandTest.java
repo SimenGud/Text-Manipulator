4649edc4d43a753c0a38b8453142a63de07a17ba
SimenGud <simeng2003@gmail.com>
2025-02-09 14:42:54 +0100

    Added CapitalizeSelection test, main method need fixing

--- src/test/java/org/BIDATA/project/capitalize/CapitalizeSelectionTextCommandTest.java
+++ src/test/java/org/BIDATA/project/capitalize/CapitalizeSelectionTextCommandTest.java
@@ -1,4 +1,20 @@
 package org.BIDATA.project.capitalize;
 
-public class CapitalizeSelectionTextCommandTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CapitalizeSelectionTextCommandTest {
+
+  @Test
+  void CapitalizeSelectionTextCommand() {
+
+    CapitalizeTextCommand command = new CapitalizeSelectionTextCommand("selection"); //TODO: Fix the method
+
+    String input = "text with selection and another selection";
+    String expectedOutput = "text with Selection and another Selection";
+
+    assertEquals(expectedOutput, command.execute(input));
+  }
+
+
 }
