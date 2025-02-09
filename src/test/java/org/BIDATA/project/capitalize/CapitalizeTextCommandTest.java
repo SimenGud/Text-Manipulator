4bfff7f8bb5b28c66cf2a459af31cb06552a6545
SimenGud <simeng2003@gmail.com>
2025-02-09 14:24:17 +0100

    Added CapitalizeTextCommand Testing class

--- src/test/java/org/BIDATA/project/capitalize/CapitalizeTextCommandTest.java
+++ src/test/java/org/BIDATA/project/capitalize/CapitalizeTextCommandTest.java
@@ -1,4 +1,19 @@
 package org.BIDATA.project.capitalize;
 
-public class CapitalizeTextCommandTest {
+import org.BIDATA.project.text.TextCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CapitalizeTextCommandTest {
+
+  @Test
+  void CapitalizeTextCommand() {
+
+    TextCommand command = new CapitalizeTextCommand();
+
+    String input = "text to be capitalized";
+    String expectedOutput = "Text to be capitalized";
+
+    assertEquals(expectedOutput, command.execute(input));
+  }
 }
