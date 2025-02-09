289181a68b026f4af5882749f2763a7e2d6698bc
SimenGud <simeng2003@gmail.com>
2025-02-09 22:06:34 +0100

    Added a simple Terminal client to test methods interactivly

--- src/main/java/org/BIDATA/project/client/TerminalClient.java
+++ src/main/java/org/BIDATA/project/client/TerminalClient.java
@@ -1,4 +1,19 @@
 package org.BIDATA.project.client;
 
+import org.BIDATA.project.capitalize.CapitalizeWordsTextCommand;
+import org.BIDATA.project.text.TextCommand;
+
+import java.util.Scanner;
+
 public class TerminalClient {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string: ");
+    String input = scanner.nextLine();
+
+    TextCommand command = new CapitalizeWordsTextCommand();
+    String result = command.execute(input);
+
+    System.out.println("Result: " + result);
+  }
 }
