33f826b1f3f50d748f415435e704346096fd868b
SimenGud <simeng2003@gmail.com>
2025-02-09 14:06:16 +0100

    Added WrapTextTesting class

--- src/test/java/org/BIDATA/project/wrap/WrapTextCommandTest.java
+++ src/test/java/org/BIDATA/project/wrap/WrapTextCommandTest.java
@@ -1,4 +1,20 @@
 package org.BIDATA.project.wrap;
 
-public class WrapTextCommandTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.BIDATA.project.text.TextCommand;
+
+class WrapTextCommandTest {
+
+  @Test
+  void WrapTextCommand() {
+
+    TextCommand command = new WrapTextCommand("<p>", "</p>");
+
+    String input = "This is a test.";
+    String expectedOutput = "<p>This is a test.</p>";
+
+    assertEquals(expectedOutput, command.execute(input));
+
+  }
 }
