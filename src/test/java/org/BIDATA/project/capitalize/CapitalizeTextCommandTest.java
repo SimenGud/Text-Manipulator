d655b03bbdec2674de87b23422a0c86b017b3fd5
SimenGud <simeng2003@gmail.com>
2025-02-10 09:30:32 +0100

    Negative Tests to capitalizeText

--- src/test/java/org/BIDATA/project/capitalize/CapitalizeTextCommandTest.java
+++ src/test/java/org/BIDATA/project/capitalize/CapitalizeTextCommandTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CapitalizeTextCommandTest {
 
   @Test
-  void CapitalizeTextCommand() {
+  void CapitalizeText() {
 
     TextCommand command = new CapitalizeTextCommand();
 
@@ -15,5 +15,25 @@ class CapitalizeTextCommandTest {
     String expectedOutput = "Text to be capitalized";
 
     assertEquals(expectedOutput, command.execute(input));
+  }
+
+  @Test
+  void negativeCapitalizeText() {
+
+    TextCommand command = new CapitalizeTextCommand();
+
+    String input = "text to be capitalized";
+    String expectedOutput = "text to be capitalized";
+
+    assertNotEquals(expectedOutput, command.execute(input));
+  }
+
+  @Test
+  void CapitalizeTextEmptyText() {
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      TextCommand command = new CapitalizeTextCommand();
+      command.execute("");
+    });
   }
 }
