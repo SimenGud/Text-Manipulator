94ddb5d5da79fd409bb503ea95fbaf41061c460c
SimenGud <simeng2003@gmail.com>
2025-02-10 09:31:11 +0100

    Negative tests to capitalizeWords

--- src/test/java/org/BIDATA/project/capitalize/CapitalizeWordsTextCommandTest.java
+++ src/test/java/org/BIDATA/project/capitalize/CapitalizeWordsTextCommandTest.java
@@ -15,4 +15,24 @@ class CapitalizeWordsTextCommandTest {
 
     assertEquals(expectedOutput, command.execute(input));
   }
+
+  @Test
+  void negativeCapitalizeWordsText() {
+
+    CapitalizeTextCommand command = new CapitalizeWordsTextCommand();
+
+    String input = "text to be capitalized";
+    String expectedOutput = "text to be capitalized";
+
+    assertNotEquals(expectedOutput, command.execute(input));
+  }
+
+  @Test
+  void CapitalizeWordsTextEmptyText() {
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      CapitalizeTextCommand command = new CapitalizeWordsTextCommand();
+      command.execute("");
+    });
+  }
 }
