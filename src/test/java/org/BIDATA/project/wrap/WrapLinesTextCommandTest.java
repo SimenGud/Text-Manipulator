3320c4dad30b065e45bade5251abd084ea769558
SimenGud <simeng2003@gmail.com>
2025-02-10 09:48:52 +0100

    Additional tests to WrapLines testing class

--- src/test/java/org/BIDATA/project/wrap/WrapLinesTextCommandTest.java
+++ src/test/java/org/BIDATA/project/wrap/WrapLinesTextCommandTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class WrapLinesTextCommandTest {
 
   @Test
-  void testWrapLines() {
+  void WrapLines() {
     WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "</p>");
 
     String input = "first line\nsecond line";
@@ -16,12 +16,46 @@ class WrapLinesTextCommandTest {
   }
 
   @Test
-  void testWrapMultipleLines() {
+  void NegativeWrapLines() {
+    WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "</p>");
+
+    String input = "first line\nsecond line";
+    String expected = "first line\nsecond line";
+
+    assertNotEquals(expected, command.execute(input));
+  }
+
+  @Test
+  void WrapMultipleLines() {
     WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "</p>");
 
     String input = "first line\nsecond line\nthird line";
     String expected = "<p>first line</p>\n<p>second line</p>\n<p>third line</p>";
 
     assertEquals(expected, command.execute(input));
+  }
+
+  @Test
+  void WrapLinesEmptyWrapOne() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      WrapLinesTextCommand command = new WrapLinesTextCommand("", "</p>");
+      command.execute("first line\nsecond line");
+    });
+  }
+
+  @Test
+  void WrapLinesEmptyWrapTwo() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "");
+      command.execute("first line\nsecond line");
+    });
+  }
+
+  @Test
+  void WrapLinesEmptyText() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      WrapLinesTextCommand command = new WrapLinesTextCommand("<p>", "</p>");
+      command.execute("");
+    });
   }
 }
