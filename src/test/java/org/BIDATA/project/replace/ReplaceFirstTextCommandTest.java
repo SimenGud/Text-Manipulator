13c1b6405df9a2764b8d384f55af85c68f67b462
SimenGud <simeng2003@gmail.com>
2025-02-10 09:25:58 +0100

    Negative tests to ReplaceFirst

--- src/test/java/org/BIDATA/project/replace/ReplaceFirstTextCommandTest.java
+++ src/test/java/org/BIDATA/project/replace/ReplaceFirstTextCommandTest.java
@@ -14,4 +14,41 @@ class ReplaceFirstTextCommandTest {
 
     assertEquals(expectedOutput, command.execute(input));
   }
+
+  @Test
+  void negativeTestReplace() {
+    ReplaceTextCommand command = new ReplaceFirstTextCommand("target", "replacement");
+
+    String input = "text with target and target";
+    String expectedOutput = "text with target and target";
+
+    assertNotEquals(expectedOutput, command.execute(input));
+  }
+
+  @Test
+  void testReplaceTestExceptionEmptyReplacement() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplaceTextCommand command = new ReplaceFirstTextCommand("target", "");
+      String input = "text with target and target";
+      command.execute(input);
+    });
+  }
+
+  @Test
+  void testReplaceTestExceptionEmptyTarget() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplaceTextCommand command = new ReplaceFirstTextCommand("", "replacement");
+      String input = "text with target and target";
+      command.execute(input);
+    });
+  }
+
+  @Test
+  void testReplaceTestExceptionEmptyText() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplaceTextCommand command = new ReplaceFirstTextCommand("target", "replacement");
+      String input = "";
+      command.execute(input);
+    });
+  }
 }
